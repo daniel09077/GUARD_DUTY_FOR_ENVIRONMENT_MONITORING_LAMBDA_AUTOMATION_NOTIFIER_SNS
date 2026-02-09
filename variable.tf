5bb5cd2ffe6699b69aa5cde2270ad6afe24669b5
DANIEL OKUGUNI <tempest@DANIELs-MacBook-Air.local>
2026-02-09 23:28:17 +0100

    Initial commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "endpointemail" {
+  default = "danielokuguni855@gmail.com"
+}
+variable "GD-publisher_SNS" {
+  default = "GD-publisher-SNS"
+}
+variable "lambda_rt" {
+  default = "python3.9"
+}
